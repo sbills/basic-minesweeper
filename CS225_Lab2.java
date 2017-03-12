bafc8c89962953abfe32ba45ae913f966fae2fb8
Sean Billideau <sbillideau@comcast.net>
2017-03-11 17:21:13 -0800

    Update CS225_Lab2.java

--- CS225_Lab2.java
+++ CS225_Lab2.java
@@ -1,57 +1,4 @@
 import java.util.Scanner;
-/* CS 225 - Intro to Computer Science II
- * File Name: CS225_Lab2.java
- * Java Programming
- * Lab 2 - Due X/XX/XXXX
- * Instructor: Dr. Dan Grissom
- * 
- * Name 1: Sean Billideau
- * Name 2: FirstName2 LastName2
- * Description: This file contains the source code for Lab 2.
- */
-
-///////////////////////////////////////////////////////////////////////////////
-// INSTRUCTIONS: Update the header above EACH LAB with the correct due date,
-// first/last names (remove the "Name 2" line if working alone) and description
-// for this specific lab. You should also update the first/last names and problem
-// number (if relevant) below in the "System.out.println" statement at the beginning
-// of the "main()" method. Failure to do so will result in lost points. DO NOT change
-// the name of the class or the autograder will give you 0 points.
-//
-// PHILOSOPHY: Lab is a chance to "get your feet wet" in a safe environment as it
-// will be the first time you'll be trying to program new concepts. Thus, as you'll
-// see below, the collaboration rules for LABS are very generous since the main idea
-// is for you to learn programming with a lot of resources. To foster this 
-// environment, you'll have access to a programming partner of your choice, your peers,
-// experienced lab technicians and your instructor.
-//
-// COLLABORATION: Students may officially work with ONE (1) partner. Both names should
-// be listed in the header and in the initial "System.out.println()" statement. When
-// you submit your lab, only one partner should submit it (again, make sure both names
-// are on all files). Students MAY seek advice and look at other students' code DURING
-// lab for tips (including students who are not your direct partner), but may NOT
-// copy/paste code from students other than your official partner.
-//
-// FINISHING & GRADING: Lab assignments must be performed in the Computer Science
-// Laboratory each week. You are required to attend every lab session. Labs usually
-// consist of several book problems (sometimes there are none) and 1-2 code problems.
-// The book problems and code should both be turned in when fully completed. 
-//
-// When you finish a code problem during lab, you will demonstrate your working program
-// to the instructor or to a lab-tech/TA on duty and you and the instructor will both
-// sign off on that problem. At that point, you will receive full credit for that problem.
-// If you did not finish one or more code problems by the end of lab, you may sign off with
-// an instructor/lab-tech/TA during the last 15 minutes of lab; in this event, you will not
-// be deducted points for not finishing , but will be graded based on your final
-// submission. Book problems will always be graded via the submission and will not be checked
-// off during lab.
-//
-// If you do not finish your lab assignment during the lab period, you and your partner may
-// only seek help with your code from lab technicians (available whenever the lab is open)
-// and the instructor to aid with any confusion; please refer to posted lab times at the
-// entrance of the Main Computer Lab to see when lab techs and computers are available.
-// You may also complete the labs on your own machine if yo like (this is preferable).
-///////////////////////////////////////////////////////////////////////////////
 
 // enum class used to represent each cell of the mine board:
 // HM = Hidden Mine, RM = Revealed Mine, HE = Hidden Empty, RE = Revealed Empty
@@ -74,7 +21,7 @@ public class CS225_Lab2
 
 		// Definition of mine board. *** DO NOT CHANGE ***
 		// The hidden mines are represented by  and the hidden empty
-		// cells with  in a 5—5 array.
+		// cells with  in a 5â€”5 array.
 		MineCell [][] mineBoard = { {he, he, he, he, he},
 									{he, hm, he, hm, he},
 									{he, he, he, he, he},
@@ -82,7 +29,7 @@ public class CS225_Lab2
 									{he, he, hm, hm, he} };
 
 		// Test your board printing functions before continuing
-		//	testBoardPrinting(mineBoard); // TODO 3: Comment out when print methods tested
+		//	testBoardPrinting(mineBoard); 
 
 		// Definition of some variables and constants
 		int currScore = 5; // Starting score is 5
@@ -151,11 +98,7 @@ public class CS225_Lab2
 
 
 
-		//////////////////////////////////////////////////////
-		// TODO 7: Deliver final results. If we've reached this point,
-		// the game is over. Write code to let the user know if they won 
-		// or lost and print the final board (remember, you have a function
-		// to print the game-over board)
+		
 
 
 	}
@@ -218,8 +161,7 @@ public class CS225_Lab2
 			System.out.println();
 		}
 
-		// TODO 2: Write code to print the final (game-over) game state
-		// as specified in the comment above
+		
 	}
 
 	/////////////////////////////////////////////////////////////////////
